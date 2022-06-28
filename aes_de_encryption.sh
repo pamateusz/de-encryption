81f4e2c3deec4d6bb7db33859f36ce2298f63fb1
pamateusz <108335281+pamateusz@users.noreply.github.com>
2022-06-28 12:07:13 +0200

    Update aes_de_encryption.sh

--- aes_de_encryption.sh
+++ aes_de_encryption.sh
@@ -4,7 +4,7 @@ echo "Encryption content:"
 date  ## echo the date at end
 start=`python -c 'import datetime; print datetime.datetime.now().strftime("%s.%f")' | cut -b1-13`
 #HERE BE CODE
-/usr/bin/time -v openssl enc -aes-256-cbc -salt -k root -in meshserv.log.1 -out file1.enc
+/usr/bin/time -v openssl enc -aes-256-cbc -salt -k <password> -in meshserv.log.1 -out file1.enc
 #end=$(date +%s)
 end=`python -c 'import datetime; print datetime.datetime.now().strftime("%s.%f")' | cut -b1-13`
 runtime=$(python -c "print(${end} - ${start})")
@@ -13,7 +13,7 @@ echo "Runtime of encryption:" $runtime
 echo "Decryption content:"
 startde=`python -c 'import datetime; print datetime.datetime.now().strftime("%s.%f")' | cut -b1-13`
 
-/usr/bin/time -v  openssl enc -d -aes-256-cbc -k root -in file1.enc -out textdecryption.txt
+/usr/bin/time -v  openssl enc -d -aes-256-cbc -k <password> -in file1.enc -out textdecryption.txt
 
 endde=`python -c 'import datetime; print datetime.datetime.now().strftime("%s.%f")' | cut -b1-13`
 runtimede=$(python -c "print(${endde} - ${startde})")
